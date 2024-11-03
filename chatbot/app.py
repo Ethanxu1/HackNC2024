d940dcf6396064631022dc2cd6ae1f4c4af8d233
Akshara Kolipaka <goldyakshu@gmail.com>
2024-11-03 10:27:02 -0500

    key change

--- chatbot/app.py
+++ chatbot/app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
 
